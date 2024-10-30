c1e2b5504a0619e2dd333f6180cb0c0548b0cc02
Germano Schneider <germanoschneider87@gmail.com>
2024-10-30 15:16:47 -0300

    added new test to the user domain model

--- src/test/java/com/example/springsecuritysample/domain/UserTest.java
+++ src/test/java/com/example/springsecuritysample/domain/UserTest.java
@@ -1,49 +1,56 @@
 package com.example.springsecuritysample.domain;
 
 import jakarta.validation.ConstraintViolationException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
     @Test
     void should_build_user_with_success() {
 
-        Assertions.assertDoesNotThrow(user()::build);
+        assertDoesNotThrow(user()::build);
     }
 
     @Test
     void should_fail_when_trying_to_build_user_without_name() {
 
-        var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> user().name("").build());
+        var exception = assertThrows(ConstraintViolationException.class, () -> user().name("").build());
 
-        Assertions.assertEquals("name: should not be blank", exception.getMessage());
+        assertEquals("name: should not be blank", exception.getMessage());
     }
 
     @Test
     void should_fail_when_trying_to_build_user_without_password() {
 
-        var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> user().password("").build());
+        var exception = assertThrows(ConstraintViolationException.class, () -> user().password("").build());
 
-        Assertions.assertEquals("password: should not be blank", exception.getMessage());
+        assertEquals("password: should not be blank", exception.getMessage());
     }
 
     @Test
     void should_fail_when_trying_to_build_user_without_city() {
 
-        var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> user().city("").build());
+        var exception = assertThrows(ConstraintViolationException.class, () -> user().city("").build());
 
-        Assertions.assertEquals("city: should not be blank", exception.getMessage());
+        assertEquals("city: should not be blank", exception.getMessage());
     }
 
     @Test
     void should_fail_when_trying_to_build_user_without_birthday() {
 
-        var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> user().birthday(null).build());
+        var exception = assertThrows(ConstraintViolationException.class, () -> user().birthday(null).build());
 
-        Assertions.assertEquals("birthday: should not be null", exception.getMessage());
+        assertEquals("birthday: should not be null", exception.getMessage());
+    }
+
+    @Test
+    void should_get_the_users_age() {
+
+        assertEquals(47, user().build().getAge());
     }
 
     private User.UserBuilder user() {
